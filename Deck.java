22e3e21cc667824cdcd96f8d635ff938bee2e58b
zackeryjbrown <zackeryjbrown@gmail.com>
2022-12-22 14:08:42 -0600

    first commit

---
+++ Deck.java
@@ -0,0 +1,11 @@
+package card_game;
+
+public class Deck {
+    public static Integer maxCards = 20;
+    public int currentCardCount;
+
+    public void deckContructor() {
+        this.currentCardCount = 20;
+
+    }
+}
